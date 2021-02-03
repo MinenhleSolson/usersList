a7f45c41bf2e5b850ca3f6c5a6f320f6a59e6e64
Minenhle Solson Cele <60825687+MinenhleSolson@users.noreply.github.com>
2021-02-03 11:19:57 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -20,16 +20,7 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
     require __DIR__.'/../storage/framework/maintenance.php';
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
+
 
 require __DIR__.'/../vendor/autoload.php';
 
